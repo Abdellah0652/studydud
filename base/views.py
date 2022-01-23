b7f300d432f7a718baf2dfb140209d982ef03018
Abdellah Targaoui <abdellah.targaoui@gmail.com>
2022-01-23 17:12:50 +0100

    create topic

--- base/views.py
+++ base/views.py
@@ -11,16 +11,14 @@ rooms = [
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room =Room.objects.get(id=pk)
+
     context = {'room': room}
-    return render(request, 'room.html', context)
+    return render(request, 'base/room.html', context)
 
 
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms': rooms}
 
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
